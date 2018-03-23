b2073abfe2f1ef1e71e10447f47f8a8b14fb9aed
Suraj Deshmukh <surajd.service@gmail.com>
2018-03-23 16:27:18 +0530

    add tenant deployment configs

--- wit-build.sh
+++ wit-build.sh
@@ -5,6 +5,7 @@ set -xe
 cd $GOPATH/src/github.com/fabric8-services/fabric8-wit
 make build
 docker build -t docker.io/fabric8/fabric8-wit --file Dockerfile.deploy .
+oc project wit-dep
 oc scale deploy wit --replicas=0
 sleep 2
 oc scale deploy wit --replicas=1
