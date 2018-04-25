793305cd83e680b5b57867cb0d3717bc63c59615
Suraj Deshmukh <surajd.service@gmail.com>
2018-04-25 14:45:26 +0530

    add create codebases

---
+++ wit-create-codebases.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [ -z $token ]; then
+    echo "Please provide the server token, by exporting variable 'token' or running export token="
+    exit 1
+fi
+
+set -xe
+
+# obtain all the spaces that are available in raw format
+output=$(curl -g -L  http://minishift.local:30000/api/spaces/?page[limit]=50 -H "Authorization: Bearer $token")
+
+# once we have that raw data now extract all the space ids
+spaces=$(echo $output | jq '.data | .[] | .id' )
+
+set +e
+
+# iterate over all the ids so we add the codebase to all of them
+for space in $spaces; do
+    # these space ids have double quotes around them so get rid of it
+    space=$(echo $space | tr -d '"')
+
+    # finally now that we have all the data make that call
+    curl -X POST \
+      http://minishift.local:30000/api/spaces/$space/codebases \
+      -H "Authorization: Bearer $token" \
+      -d '{ 
+            "data": { 
+                "attributes": { 
+                    "type": "git",
+                    "url": "https://github.com/surajssd/byebyeworld"
+                },
+                "type": "codebases"
+            }
+        }'
+
+done
